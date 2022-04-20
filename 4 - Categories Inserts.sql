4241700bda9dbc1d93593ee0b6974437c29a637f
Uayeb Caballero <uayeb.caballero@gmail.com>
2022-04-19 20:24:32 -0600

    fixing characters

--- 4 - Categories Inserts.sql
+++ 4 - Categories Inserts.sql
@@ -1,10 +1,10 @@
 use miprimeradb;
 
-insert into Categories() values(1,"Beverages","Soft drinks, coffees, teas, beers, and ales");
-insert into Categories() values(2,"Condiments","Sweet and savory sauces, relishes, spreads, and seasonings");
-insert into Categories() values(3,"Confections","Desserts, candies, and sweet breads");
-insert into Categories() values(4,"Dairy Products","Cheeses");
-insert into Categories() values(5,"Grains/Cereals","Breads, crackers, pasta, and cereal");
-insert into Categories() values(6,"Meat/Poultry","Prepared meats");
-insert into Categories() values(7,"Produce","Dried fruit and bean curd");
-insert into Categories() values(8,"Seafood","Seaweed and fish");
+insert into Categories values(1,'Beverages','Soft drinks, coffees, teas, beers, and ales');
+insert into Categories values(2,'Condiments','Sweet and savory sauces, relishes, spreads, and seasonings');
+insert into Categories values(3,'Confections','Desserts, candies, and sweet breads');
+insert into Categories values(4,'Dairy Products','Cheeses');
+insert into Categories values(5,'Grains/Cereals','Breads, crackers, pasta, and cereal');
+insert into Categories values(6,'Meat/Poultry','Prepared meats');
+insert into Categories values(7,'Produce','Dried fruit and bean curd');
+insert into Categories values(8,'Seafood','Seaweed and fish');
